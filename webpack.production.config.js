8d2f9e4a5390ede07d75dce09d9a1c4e20c263aa
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-14 12:55:09 +0200

    Add shorthand loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -45,7 +45,7 @@ module.exports = {
       include: path.resolve('src/app')
     }, {
       test: /(\.scss|\.css)$/,
-      loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
+      loader: ExtractTextPlugin.extract('style', 'css?minimize&sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass!shorthand')
     }]
   },
   postcss: [autoprefixer]
