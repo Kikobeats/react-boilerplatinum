e6cff378e2cb1aa5dd25f06f77d3ce4274fcdc47
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-17 16:32:04 +0200

    WIP

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const WebpackDevServer = require('webpack-dev-server')
-const config = require('./webpack.config')
+const config = require('./webpack.dev.config')
 const webpack = require('webpack')
 
 new WebpackDevServer(webpack(config), {
