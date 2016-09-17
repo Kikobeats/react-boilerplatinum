8a893f1b3940e8167c5132c603b6f94f015ca92d
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-17 18:35:13 +0200

    Add html minification rules

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,21 @@ module.exports = {
       hash: true,
       inject: true,
       minify: {
+        collapseWhitespace: true,
+        decodeEntities: true,
+        html5: true,
+        processConditionalComments: true,
+        removeAttributeQuotes: true,
         removeComments: true,
-        collapseWhitespace: true
+        removeEmptyAttributes: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeTagWhitespace: true,
+        sortAttributes: true,
+        sortClassName: true,
+        useShortDoctype: true
       }
     })),
     new HtmlWebpackHarddiskPlugin(),
