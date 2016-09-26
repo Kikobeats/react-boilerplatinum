5b0f3963bb353fe467db2538e878681d22943064
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-26 21:22:34 +0200

    WIP

--- src/app/Layout/index.js
+++ src/app/Layout/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Layout ({ children }) {
   return (
-  <div>
-    <h1>Hello, world!</h1>
-    {children}
-  </div>
+    <section class='mw5 mw7-ns mt6 center bg-light-gray pa3 ph5-ns'>
+      <h1 class='mt0 f3 tc'>Tachyons</h1>
+      <p class='f5 tc lh-copy'>
+        <h1>Hello, world!</h1>
+        {children}
+      </p>
+    </section>
   )
 }
