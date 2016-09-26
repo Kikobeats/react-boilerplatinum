1a0671eaa2f45aa42a02aea8b1086c9187a5bcbf
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-26 21:41:11 +0200

    Remove sass in favour of postcss

--- src/app/Layout/index.js
+++ src/app/Layout/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Layout ({ children }) {
   return (
-    <section class='mw5 mw7-ns mt6 center bg-light-gray pa3 ph5-ns'>
-      <h1 class='mt0 f3 tc'>Tachyons</h1>
-      <p class='f5 tc lh-copy'>
+    <section className='mw5 mw7-ns mt6 center bg-light-gray pa3 ph5-ns'>
+      <h1 className='mt0 f3 tc'>React Boilerplatinum</h1>
+      <p className='f5 tc lh-copy'>
         <h1>Hello, world!</h1>
         {children}
       </p>
