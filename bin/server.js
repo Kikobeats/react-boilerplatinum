27024f2ecc81fba55a3805b0dd4211baedfaabdb
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-26 15:42:26 +0200

    Little refactor, more readable

--- bin/server.js
+++ bin/server.js
@@ -4,11 +4,15 @@ const WebpackDevServer = require('webpack-dev-server')
 const config = require('../webpack.dev.config')
 const webpack = require('webpack')
 
-new WebpackDevServer(webpack(config), {
+var compiler = webpack(config)
+
+var server = new WebpackDevServer(compiler, {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
   contentBase: 'src/www'
-}).listen(3000, 'localhost', function (err) {
+})
+
+server.listen(3000, 'localhost', function (err) {
   if (err) throw err
 })
