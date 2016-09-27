e4d59ab7bf398f978c48635e2bdb98eeb0d372da
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-27 17:52:19 +0200

    Little main scaffold refactor

--- src/app/Layout/index.js
+++ src/app/Layout/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 export default function Layout ({ children }) {
   return (
-    <section className='mw5 mw7-ns mt6 center bg-light-gray pa3 ph5-ns'>
-      <h1 className='mt0 f3 tc'>React Boilerplatinum</h1>
-      <p className='f5 tc lh-copy'>
+    <article className='mw5 mw7-ns mt6 center bg-light-gray pa3 ph5-ns'>
+      <section className='f5 tc lh-copy'>
         <h1>Hello, world!</h1>
         {children}
-      </p>
-    </section>
+      </section>
+    </article>
   )
 }
